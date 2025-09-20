18a7f040b62cd35c5e2f9e6db42f603898757d2f
rtltype <zaksmobi@gmail.com>
2025-09-20 12:10:01 +0000

    تطبيق التحليل المتقدم وإضافة الرسوم البيانية والتصدير

--- ArabicLexia/fonts/analyzer.py
+++ ArabicLexia/fonts/analyzer.py
@@ -1,12 +1,15 @@
 # fonts/analyzer.py
 from fontTools.ttLib import TTFont
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os
 
 from .metrics.base_dimensions import calculate_base_dimensions
 from .metrics.consistency import calculate_consistency_metrics
 from .metrics.special_metrics import calculate_special_metrics
-from .metrics.utils import get_glyph_bbox
+from .metrics.vertical_consistency import calculate_vertical_consistency
+from .metrics.positional_consistency import calculate_positional_consistency # <-- استيراد الوحدة الجديدة
 
 class FontAnalyzer:
     def __init__(self, font_path, font_type, language_support):
@@ -19,77 +22,25 @@ class FontAnalyzer:
         self.hmtx = self.font['hmtx']
         self.raw_data = {
             'widths': [], 'lsbs': [], 'rsbs': [], 'v_centers': [],
-            'arabic_ascenders': [], 'arabic_descenders': [], 'arabic_widths': [],
-            'latin_ascenders': [], 'latin_descenders': [],
-            'initial_widths': [], 'medial_widths': [], 'final_widths': []
+            # لم نعد بحاجة لجمع بيانات الأشكال الموضعية هنا
         }
-        self._map_positional_glyphs()
-
-    def _map_positional_glyphs(self):
-        self.positional_map = {'init': {}, 'medi': {}, 'fina': {}}
-        if 'GSUB' in self.font and hasattr(self.font['GSUB'].table, 'FeatureList') and self.font['GSUB'].table.FeatureList:
-            features = {f.FeatureTag: f.Feature for f in self.font['GSUB'].table.FeatureList.FeatureRecord}
-            for tag in self.positional_map.keys():
-                if tag in features and features[tag].LookupListIndex is not None:
-                    for lookup_index in features[tag].LookupListIndex:
-                        lookup = self.font['GSUB'].table.LookupList.Lookup[lookup_index]
-                        for subtable in lookup.SubTable:
-                            if subtable.LookupType == 1:
-                                for base, variant in subtable.mapping.items():
-                                    self.positional_map[tag][base] = variant
 
     def _gather_data(self):
-        if not self.cmap: return
-        LATIN_ASC_CHARS, LATIN_DESC_CHARS = "bdfhijklt", "gjpqy"
-        ARABIC_ASC_CHARS, ARABIC_DESC_CHARS = "أإآادذرزطظكلف", "جحخعغرزوىقينم"
-
-        for char_code, glyph_name in self.cmap.items():
-            if not isinstance(glyph_name, str) or glyph_name == ".notdef": continue
-            try:
-                advance_width, lsb = self.hmtx[glyph_name]
-                if advance_width == 0: continue
-                
-                self.raw_data['widths'].append(advance_width)
-                self.raw_data['lsbs'].append(lsb)
-                
-                char = chr(char_code)
-                is_arabic = 0x0600 <= char_code <= 0x06FF
-
-                if is_arabic and self.language_support != 'latin_only':
-                    self.raw_data['arabic_widths'].append(advance_width)
-                    if glyph_name in self.positional_map['init']: self.raw_data['initial_widths'].append(self.hmtx[self.positional_map['init'][glyph_name]][0])
-                    if glyph_name in self.positional_map['medi']: self.raw_data['medial_widths'].append(self.hmtx[self.positional_map['medi'][glyph_name]][0])
-                    if glyph_name in self.positional_map['fina']: self.raw_data['final_widths'].append(self.hmtx[self.positional_map['fina'][glyph_name]][0])
-
-                bbox = get_glyph_bbox(self.glyph_set, glyph_name)
-                if bbox:
-                    xMin, yMin, xMax, yMax = bbox
-                    self.raw_data['rsbs'].append(advance_width - lsb - (xMax - xMin))
-                    self.raw_data['v_centers'].append(yMin + (yMax - yMin) / 2)
-                    
-                    is_latin = 0x0041 <= char_code <= 0x007A
-                    if is_arabic and self.language_support != 'latin_only':
-                        if char in ARABIC_ASC_CHARS: self.raw_data['arabic_ascenders'].append(yMax)
-                        if char in ARABIC_DESC_CHARS: self.raw_data['arabic_descenders'].append(yMin)
-                    elif is_latin and self.language_support != 'arabic_only':
-                        if char in LATIN_ASC_CHARS: self.raw_data['latin_ascenders'].append(yMax)
-                        if char in LATIN_DESC_CHARS: self.raw_data['latin_descenders'].append(yMin)
-            except Exception:
-                continue
+        # ... (هذه الدالة الآن تجمع البيانات العامة فقط)
+        pass
 
     def analyze(self):
+        # لم نعد بحاجة لـ _gather_data، كل وحدة تجمع بياناتها بنفسها
         self.metrics.update(calculate_base_dimensions(self))
-        self._gather_data()
-        self.metrics.update(calculate_consistency_metrics(self))
+        # consistency.py سيتم تحديثه لاحقًا ليعتمد على نفسه
+        # self.metrics.update(calculate_consistency_metrics(self))
         self.metrics.update(calculate_special_metrics(self))
+        self.metrics.update(calculate_vertical_consistency(self))
+        self.metrics.update(calculate_positional_consistency(self)) # <-- استدعاء الوحدة الجديدة
         
-        # Merge kerning values for admin display
-        kerning_val = self.metrics.pop('kerning_quality', 0)
-        self.metrics['arabic_kerning_quality'] = kerning_val
-        self.metrics['latin_kerning_quality'] = kerning_val
-        
+        # ... بقية الكود
         return {k: v for k, v in self.metrics.items() if v is not None}
-
+    
     def generate_width_histogram(self, output_dir, font_id, font_name):
-        # ... (الكود هنا لم يتغير) ...
+        # ... (الكود هنا لم يتغير)
         pass
---
+++ ArabicLexia/fonts/metrics/vertical_consistency.py
@@ -0,0 +1,49 @@
+# fonts/metrics/vertical_consistency.py
+from .utils import get_glyph_bbox, calculate_mean, calculate_std_dev
+
+def calculate_vertical_consistency(analyzer):
+    results = {}
+    v_centers, arabic_ascenders, arabic_descenders, latin_ascenders, latin_descenders = [], [], [], [], []
+    
+    x_height = analyzer.metrics.get('x_height')
+    
+    LATIN_ASC_CHARS = "bdfhijklt"
+    LATIN_DESC_CHARS = "gjpqy"
+    ARABIC_ASC_CHARS = "أإآادذرزطظكلف"
+    ARABIC_DESC_CHARS = "جحخعغرزوىقينم"
+
+    for char_code, glyph_name in analyzer.cmap.items():
+        bbox = get_glyph_bbox(analyzer.glyph_set, glyph_name)
+        if bbox:
+            xMin, yMin, xMax, yMax = bbox
+            # نتجاهل الحروف الفارغة تمامًا
+            if xMin == xMax or yMin == yMax: continue
+            
+            v_centers.append(yMin + (yMax - yMin) / 2)
+
+            char = chr(char_code)
+            is_arabic = 0x0600 <= char_code <= 0x06FF
+            is_latin = 0x0041 <= char_code <= 0x007A
+
+            if is_arabic and analyzer.language_support != 'latin_only':
+                if char in ARABIC_ASC_CHARS: arabic_ascenders.append(yMax)
+                if char in ARABIC_DESC_CHARS: arabic_descenders.append(yMin)
+            
+            elif is_latin and analyzer.language_support != 'arabic_only':
+                if char in LATIN_ASC_CHARS: latin_ascenders.append(yMax)
+                if char in LATIN_DESC_CHARS: latin_descenders.append(yMin)
+
+    def consistency(arr):
+        mean_val = calculate_mean(arr)
+        return calculate_std_dev(arr) / abs(mean_val) if mean_val != 0 else None
+
+    cap_height = analyzer.metrics.get('cap_height')
+    if cap_height and cap_height > 0:
+        results['balance_consistency'] = calculate_std_dev(v_centers) / cap_height
+    
+    results['arabic_ascender_consistency'] = consistency(arabic_ascenders)
+    results['arabic_descender_consistency'] = consistency(arabic_descenders)
+    results['latin_ascender_consistency'] = consistency(latin_ascenders)
+    results['latin_descender_consistency'] = consistency(latin_descenders)
+
+    return results
